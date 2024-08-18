5aa9afd637a582e869b9d8b4952fdc419bb01637
Uros <mirkovic.uk@gmail.com>
2024-08-18 14:28:05 +0100

    update dot string

--- src/gdpr_obfuscation.py
+++ src/gdpr_obfuscation.py
@@ -2,23 +2,30 @@ from urllib.parse import urlparse
 import csv
 import boto3
 
-def gdpr_obfuscator(file_to_obfuscate:str, pii_fields:list):
+def gdpr_obfuscator(file_path:str, pii_fields:list):
     """
-    some dot str here
+    Retrieve data ingested to AWS S3 and
+    intercept personally identifiable information (PII).
+    Return a byte stream object containing an exact 
+    copy of the input file but with the sensitive
+    data replaced with obfuscated strings.
+
+    :param: file_path (str) S3 location of the data file for obfuscation
+    :param: pii_fields list of the names of the fields that are 
+        required to be obfuscated
+    :return: TBC <-----
     """
-    o = urlparse(file_to_obfuscate)
-    bucket, key = 's3://' + o.netloc, o.path
-    new_string = bucket + key
-    print(new_string == file_to_obfuscate)
-    print(new_string)
+    bucket, key = get_bucket_and_key(file_path)
+    data = 
+
 
 def get_bucket_and_key(s3_file_path:str):
     """
     Extract S3 bucket name and key of the object from s3 file path
     expect file path like 's3://my_backet/some_folder/file.txt
     
-    :param: s3_file_path (string) 
-    :retunr: bucket, key 
+    :param: s3_file_path (str) path to data on AWS s3
+    :retunr: bucket , key (str) 
     """
     
     o = urlparse(s3_file_path, allow_fragments=False)
