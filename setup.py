a4338a6f21a79e6fd1528940e6838d764fd05f53
Uros <mirkovic.uk@gmail.com>
2024-08-25 17:28:38 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-    name = "GDPR-Obfuscator",
+    name = "GDPR_Obfuscator",
     version = "2024.0.0",
-    packages=['gdpr_obfuscation']
+    # packages=['gdpr_obfuscation']
 )
